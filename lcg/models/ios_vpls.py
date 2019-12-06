2148d154158e25d5a6002499174d13f5d895c072
byt3-m3 <cbaxtertech@gmail.com>
2019-12-06 14:32:35 +0300

    Add Schema for VFI, Add Schema for VFIPeers

--- lcg/models/ios_vpls.py
+++ lcg/models/ios_vpls.py
@@ -3,8 +3,18 @@ from marshmallow import Schema, fields
 VPLS_ENCAPSULATION_TYPES = ["DOT1Q"]
 
 
-# TODO: Add Schema for VFI
 # TODO: Add Schema for VFI-Peers
+class IOSVFIPeersSchema(Schema):
+    remote_addr = fields.Str()
+
+
+# TODO: Add Schema for VFI
+class IOSVFISchema(Schema):
+    name = fields.Str(required=True)
+    vfi_id = fields.Str(required=True)
+    vfi_peers = fields.Nested(IOSVFIPeersSchema)
+
+
 # TODO: Add Schema for bridge-domain
 # TODO: Add Schema for bridge-domain members
 
@@ -14,7 +24,7 @@ class IOSVPLSEncapsulationSchema(Schema):
     p_tag = fields.Int()
 
 
-class IOSVPLSEFPSchema(Schema):
+class IOSEFPSchema(Schema):
     link_id = fields.Str(required=True)
     instance_id = fields.Str(required=True)
     encapsulation = fields.Nested(IOSVPLSEncapsulationSchema)
