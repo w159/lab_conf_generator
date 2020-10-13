9830f9a143fab7b734118e2c74aab6276a30c988
byt3-m3 <cbaxtertech@gmail.com>
2020-10-13 01:50:29 -0400

    Remove AWS Keys from REPO

--- gcg/env.py
+++ gcg/env.py
@@ -7,8 +7,8 @@ DB_PORT = os.getenv("DB_PORT", 27017)
 DB = os.getenv("DB", "LCG_API")
 DEBUG = os.getenv("DEBUG", True)
 
-AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY",'AKIA6MRXVMB5NBSTYHO2')
-AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY", '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')
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 
 TEMPLATE_FOLDER = os.getenv('TEMPLATE_FOLDER', 'template')
 
