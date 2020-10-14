c3a6660321dd8195cb4b17f4d695ede805292654
byt3-m3 <cbaxtertech@gmail.com>
2020-10-14 07:53:55 -0400

    changed the RESTFul interface to accept new data model and controller_gcg.

--- gcg/api/resources/resource_gcg.py
+++ gcg/api/resources/resource_gcg.py
@@ -1,12 +1,11 @@
-from flask_restful import Resource, request
-from gcg.api.controllers.controller_gcg import controller_gcg, Task, ControllerResult
-from gcg.utils import make_json_response, make_text_response
-from gcg.schemas import schema_request
-from marshmallow.exceptions import ValidationError
+import http.client as http_status_codes
 from datetime import datetime
-import json
-
 from distutils.util import strtobool
+from flask_restful import Resource, request
+from gcg.api.controllers.controller_gcg import controller_gcg_v2, ControllerResult
+from gcg.exceptions import GCGValidationError
+from gcg.schemas import schema_request
+from gcg.utils import make_json_response, make_text_response
 
 
 class GCGResource(Resource):
@@ -15,50 +14,56 @@ class GCGResource(Resource):
     def post():
         request_schema = schema_request.GCGAPIOpts()
 
-        json_data = request.json
-
-        url_params = request.args
+        # Extract Header Info
+        AWS_ACCESS_KEY = request.headers['X-Api-Key']
+        AWS_SECRET_KEY = request.headers['X-Api-Secret']
+        USER_AGENT = request.headers['User-Agent']
 
         # Extracts URL params
+        url_params = request.args
+
         return_type = url_params.get("return_type", "json")
-        store_aws = url_params.get("return_type", False)
+        store_aws = bool(strtobool(url_params.get("store_aws", 'false')))
         template_type = url_params.get("template_type")
-        name = url_params.get("name")
-        lab_name = url_params.get("lab_name")
+        name = url_params.get("name", f'GCG_API_{str(datetime.now().strftime("%m_%d_%Y_%H_%M_%S"))}')
+        lab_name = url_params.get("lab_name", f'GCG_API_{str(datetime.now().strftime("%m_%d_%Y"))}')
 
-        # Extracts the request options in the JSON body
-        request_opts = json_data.get("opts")
-        if request_opts is None:
-            return make_json_response({}, "Missing json body opts", status_code=409)
+        # Extracts JSON Body
 
+        json_data = request.json
+
+        # Generate Config
         try:
-            request_opts = request_schema.loads(json.dumps(request_opts))
-            if not request_opts.get("name"):
-                name = f'GCG_API_{str(datetime.now().strftime("%m_%d_%Y_%H_%M_%S"))}'
+            result = controller_gcg_v2(
+                data=json_data,
+                store_aws=store_aws,
+                store_local=False,
+                template_type=template_type,
+                lab_name=lab_name,
+                name=name,
+            )
+
+            if isinstance(result, ControllerResult):
+                if return_type == "text":
+                    return make_text_response(
+                        data=result.data,
+                        msg="Success",
+                        status_code=http_status_codes.OK
+                    )
+                else:
+                    return make_json_response(
+                        data=result.data,
+                        msg="Success",
+                        status_code=http_status_codes.OK
+                    )
             else:
-                name = request_opts.get("name")
+                return
 
-            if not request_opts.get("lab_name"):
-                lab_name = f'GCG_API_{str(datetime.now().strftime("%m_%d_%Y"))}'
-            else:
-                lab_name = request_opts.get("lab_name")
+        except GCGValidationError as err:
+            return make_json_response(
+                data={},
+                msg=f"Validation Error: {str(err)}",
+                status_code=http_status_codes.CONFLICT
+            )
 
-        except ValidationError as error:
-            return make_json_response(data={}, msg=f"Option Validation Error: {str(error)}", status_code=409)
 
-        store_aws = request_opts.get("store_aws", False)
-
-        result = controller_gcg(json_data=json_data, store_aws=store_aws, lab_name=lab_name, name=name)
-
-        if isinstance(result, ControllerResult):
-            if return_type == "text":
-
-                return make_text_response(result.data, result.msg, result.status)
-
-            elif return_type == "json":
-                return make_json_response(result.data, result.msg, result.status)
-
-            elif return_type == "list":
-                return make_json_response(result.data.split("\n"), result.msg, result.status)
-
-        return make_json_response(None, "Unsupported Template Type", 400)
